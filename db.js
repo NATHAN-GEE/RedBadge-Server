fa94870195ad9a0cbc3ff2052c54ae5c8be442ca
FirstName LastName <nathangee867@gmail.com>
2021-08-27 13:19:04 -0400

    updates

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.ROMAN, {
-    dialect: 'postgres',
+  dialect: "postgres",
+  ssl: process.env.ENVIRONMENT === "production",
 });
 
 module.exports = sequelize;
